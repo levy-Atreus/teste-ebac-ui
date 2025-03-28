750d70318138a5487fb58f2ac1789689558e9ee9
Levy Atreus <levyatreus@gmail.com>
2025-03-28 10:56:24 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('fabio.teste@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('cotain' , 'olá, fabio.teste (não é fabio.teste? Sair)')
+    })
+
+    
+})
